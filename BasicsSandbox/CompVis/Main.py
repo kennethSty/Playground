81c379659c03cc7a8e0afc4e966d7cd55dd2632f
kennethSty <kenneth.styppa@web.de>
2025-02-13 09:20:56 +0100

    Remove BasicsSandbox

--- BasicsSandbox/CompVis/Main.py
+++
@@ -1,9 +0,0 @@
-from Data import DataGenerator
-
-def main():
-    data = DataGenerator()
-    data.visualize_by_index(0, "train")
-
-
-if __name__ == "__main__":
-    main()
--- BasicsSandbox/TorchBasics/PreProcessingEngine.py
+++
@@ -1,9 +0,0 @@
-from sklearn.model_selection import train_test_split
-
-class PreProcessingEngine:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def get_split(self, test_size, random_state = 42):
-        return train_test_split(self.x, self.y, test_size = test_size, random_state = random_state)
